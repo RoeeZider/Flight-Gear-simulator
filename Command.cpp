daecb8c0e91ef1a892b88581c5870c18432741fd
harel-feldman <harel.feldman1@live.biu.ac.il>
2019-12-17 15:17:35 +0200

    first program

--- Command.cpp
+++ Command.cpp
@@ -2,4 +2,55 @@
 // Created by roee on 17/12/2019.
 //
 
+#include <sys/socket.h>
+#include <bits/socket_type.h>
+#include <bits/socket.h>
+#include <netinet/in.h>
 #include "Command.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unordered_map>
+
+
+int OpenServerCommand::execute(vector<string> vec) {
+    int port = stoi(vec[1]);
+    stringstream str;
+    string s;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    int new_socket;
+    int valread;
+    char buffer[1024] = {0};
+    bool flag = true;
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(port);
+    int b = bind(server_fd, (struct sockaddr *) &address, sizeof(address));
+    int l = listen(server_fd, 3);
+    while (flag == true) {
+        new_socket = accept(server_fd, (struct sockaddr *) &address,
+                            (socklen_t *) &addrlen);
+        valread = read(new_socket, buffer, 1024);
+        printf("%s\n", buffer);
+        str<<buffer;
+        s=str.str();
+        for(auto it=parameters.begin();it<parameters.end();it++){
+            int pos=s.find(",");
+            int val=stoi(s.substr(0,pos-1));
+            parameters.it->second=val;
+
+
+
+        }
+
+        //send(new_socket, hello, strlen(hello), 0);
+        // printf("Hello message sent\n");
+        return 0;
+    }
+
+}
+
--- main.cpp
+++ main.cpp
@@ -1,10 +1,123 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <map>
+
+using namespace std;
+
+static map<string, double> parameters;
+
+void lexer(vector<string> &arr, string line);
+
+void build_parameters_map();
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    build_parameters_map();
+    vector<string> commands;
+    //change for main argv[1]
+    std::ifstream file("fly.txt");
+    if (file.is_open()) {
+        string line;
+        while (getline(file, line)) {
+            lexer(commands, line);
+        }
+        file.close();
+    } else {
+        cout << "Error in opening file" << endl;
+    }
+
+    std::cout << "myvector contains:";
+    for (std::vector<string>::iterator it = commands.begin(); it != commands.end(); ++it)
+        std::cout << ' ' << *it;
+    std::cout << '\n';
+
     return 0;
 }
 
+void lexer(vector<string> &arr, string line) {
+    int j = 0;
+    string word;
+    string word2;
+    vector<string> vec2;
+    for (int i = 0; i < line.length(); i++) {
+        while (line[i] != ' ' && line[i] != '(') {
+            word = word + line[i];
+            i++;
+        }
+        break;
+    }
 
-//ctrl k + ctrlaltk
-//harel need to clone
+    if (word.compare("Print") == 0) {
+        for (int i = 5; i < line.length(); i++) {
+            if (line[i] == '(') {
+                while (i < line.length() - 2) {
+                    i++;
+                    word2 = word2 + line[i];
+
+                }
+            }
+        }
+        arr.push_back("Print");
+        arr.push_back(word2);
+        arr.push_back("EOL\n");
+        word2 = "";
+    }
+        //maybe in the regular case
+
+
+    else if (word.compare("while") == 0) {
+        for (int i = 6; i < line.length(); i++) {
+            while (line[i] != '{') {
+                //check spaces
+                word2 = word2 + line[i];
+                i++;
+            }
+        }
+        arr.push_back("while");
+        word2 = word2.substr(0, word2.length() - 1);
+        arr.push_back(word2);
+        arr.push_back("{");
+        arr.push_back("EOL\n");
+        word2 = "";
+    } else {
+        for (int i = 0; i < line.length(); i++) {
+            while (line[i] != ' ' && line[i] != '(' && line[i] != ')' && line[i] != '\n') {
+                if (i >= line.length()) {
+                    break;
+                }
+                word2 = word2 + line[i];
+                i++;
+            }
+            arr.push_back(word2);
+            word2 = "";
+        }
+        arr.push_back("EOL\n");
+    }
+}
+
+void build_parameters_map() {
+    parameters["airspeed-indicator_indicated-speed-kt"] = 0;
+    parameters["altimeter_indicated-altitude-ft"] = 0;
+    parameters["altimeter_pressure-alt-ft"] = 0;
+    parameters["attitude-indicator_indicated-pitch-deg"] = 0;
+    parameters["attitude-indicator_indicated-roll-deg"] = 0;
+    parameters["attitude-indicator_internal-pitch-deg"] = 0;
+    parameters["attitude-indicator_internal-roll-deg"] = 0;
+    parameters["encoder_indicated-altitude-ft"] = 0;
+    parameters["encoder_pressure-alt-ft"] = 0;
+    parameters["gps_indicated-altitude-ft"] = 0;
+    parameters["gps_indicated-ground-speed-kt"] = 0;
+    parameters["gps_indicated-vertical-speed"] = 0;
+    parameters["indicated-heading-deg"] = 0;
+    parameters["magnetic-compass_indicated-heading-deg"] = 0;
+    parameters["slip-skid-ball_indicated-slip-skid"] = 0;
+    parameters["turn-indicator_indicated-turn-rate"] = 0;
+    parameters["vertical-speed-indicator_indicated-speed-fpm"] = 0;
+    parameters["flight_aileron"] = 0;
+    parameters["flight_elevator"] = 0;
+    parameters["flight_rudder"] = 0;
+    parameters["flight_flaps"] = 0;
+    parameters["engine_throttle"] = 0;
+    parameters["engine_rpm"] = 0;
+}
